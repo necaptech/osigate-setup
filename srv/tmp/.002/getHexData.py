b5d3322befe4f2eae01f0c72e934bcb5da9dcbd7
necaptech <119735052+necaptech@users.noreply.github.com>
2022-12-10 16:14:56 +0100

    Update getHexData.py

--- srv/tmp/.002/getHexData.py
+++ srv/tmp/.002/getHexData.py
@@ -6,9 +6,7 @@ import MySQLdb
 import time
 
 conn = MySQLdb.connect(host= "localhost",
-                  user="root",
-                  passwd="root",
-                  db="TECNOQ")
+    user="root", passwd="root", db="TECNOQ")
 x = conn.cursor()
 s_port = '/dev/ttyS0'
 #s_port = '/dev/ttyUSB0'
@@ -17,36 +15,37 @@ b_rate = 1200
 def read_serial(ser):
     c = 0
     while True:
-      inp=''
-      try:
-        inp = ser.read(size=71) 
-        if inp:
-          c+=1
-          print " [*] inp = %s" % inp
-          if inp[0] == 'R':
-              print ("Message %d from osiRELE" % c)
-              x.execute('''INSERT into OSIRE_STATUS (status) values (%s)''',[(inp.encode("hex"))])
-          else:
-              print ("Message %d from osiNode" % c)
-              x.execute('''INSERT into HEX_INPUT_TB (HEXSTR) values (%s)''',[(inp.encode("hex"))])
-          conn.commit()
-      except Exception, e:
-          f = open("/var/log/getHexData.log", "a")
-          f.write("%s RX Error: %s, message: %s\n" % (time.asctime(), str(e), inp))
-          f.close()
-	  exit()
-      except:
-          f = open("/var/log/getHexData.log", "a")
-          f.write("%s Unknown RX Error! Message: %s\n" % (time.asctime(), inp))
-          f.close()
-          exit()
+        inp=''
+        try:
+            inp = ser.read(size=71) 
+            if inp:
+                c+=1
+                print (" [*] inp = %s" % inp)
+                if inp[0] == 'R':
+                    print ("Message %d from OsiRELE" % c)
+                    x.execute('''INSERT into OSIRE_STATUS (status) values (%s)''',[inp.hex()])
+                else:
+                    print ("Message %d from OsiNODE" % c)
+                    print (inp)
+                    print ('''INSERT into HEX_INPUT_TB (HEXSTR) values (%s)''',[inp.hex()])
+                    x.execute('''INSERT into HEX_INPUT_TB (HEXSTR) values (%s)''',[inp.hex()])
+                conn.commit()
+        except Exception as e:
+            f = open("/var/log/getHexData.log", "a")
+            f.write("%s RX Error: %s, message: %s\n" % (time.asctime(), str(e), inp))
+            f.close()
+            continue
+        except:
+            f = open("/var/log/getHexData.log", "a")
+            f.write("%s Unknown RX Error! Message: %s\n" % (time.asctime(), inp))
+            f.close()
+            continue
 
 ser = serial.Serial(
     port=s_port,
     baudrate=b_rate,
     timeout=0.05
 )
-
 
 print(">>> Receiving messages on radio interface from port %s ..." % s_port)
 
