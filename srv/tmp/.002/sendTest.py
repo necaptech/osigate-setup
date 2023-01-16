8ec9d954a0a06189c98944169de5fc540c955a7f
necaptech <119735052+necaptech@users.noreply.github.com>
2023-01-16 19:59:51 +0100

    Update sendTest.py

--- srv/tmp/.002/sendTest.py
+++ srv/tmp/.002/sendTest.py
@@ -11,11 +11,11 @@ ser = serial.Serial(
     write_timeout=1
 )
 
-while True:
-    byteForm = bytes.fromhex("5230303032370000ff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000024a0ff0")
+for index in range(3):
+    byteForm = bytes.fromhex("5230303032370000ff0001000000000000000000000000000000000000000000024b0ff0")
     print(byteForm)
     print(ser.write(byteForm))
-    sleep(5)
+    sleep(3)
 
 # 0FF0 to send, F00F to receive
 # Need to send five times when changed, sent += 1 only when sent this, priority to older.
