8c94214577ca77fef6f95bbfc14bfd2a536b43cd
necaptech <119735052+necaptech@users.noreply.github.com>
2023-03-31 23:06:02 +0200

    Update getHexData.py

--- srv/tmp/.002/getHexData.py
+++ srv/tmp/.002/getHexData.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
+from datetime import datetime, timedelta
 from subprocess import call
-from datetime import datetime
 from random import random
 import serial
 import MySQLdb
@@ -33,19 +33,22 @@ until = 'until'
 delayend = 'delayend'
 startRange = 'rStart'
 endRange = 'rEnd'
+maxTries = 8
 
 # Get Rules
 rules = []
 relesStatus = {}
+recentTries = {}
 try:
     if (os.path.exists('/srv/data/osiRele')):
         with open('/srv/data/osiRele', "r") as relesNamesFile:
             relesNames = [x.replace("\n", "") for x in relesNamesFile.readlines()]
             for releName in relesNames:
                 if releName:
+                    recentTries[releName] = 0
                     relesStatus[releName] = {}
                     for relPin in range(1, 9):
-                        relesStatus[releName][relPin] = {onoff: 0, duration: 0, until: 0, delayend: 0}
+                        relesStatus[releName][relPin] = {onoff: 0, until: 0, delayend: 0}
                         pinFileName = '/srv/data/' + releName + '.' + str(relPin)
                         if (os.path.exists(pinFileName)):
                             with open(pinFileName, "r") as pinFile:
@@ -93,7 +96,7 @@ def read_serial(ser):
 
     # Init Misc
     mustBeUpdated = []
-    repeatSend = False
+    updatingOtherReles = False
     
     # timeA = timeB = 0
     while True:
@@ -110,12 +113,13 @@ def read_serial(ser):
                 # f.write("%s\n%s\n%s\n\n" % (str(datetime.now()), inp, inp.hex()))
                 # f.close()
 
-                f = open("/var/www/html/log.log", "a")
-                f.write("IN: %s (%s)\n" % (inp.hex(), str(datetime.now())))
-                f.close()
+                f = open("/var/www/html/log.log", "a") # TMP
+                f.write("IN: %s (%s)\n" % (inp.hex(), str(datetime.now()))) # TMP
+                f.close() # TMP
 
                 # print(inp.hex())
                 
+                x = conn.cursor()
                 x.execute('''INSERT into HEX_INPUT_TB (HEXSTR) values (%s)''',[inp.hex()])
                 conn.commit()
 
@@ -123,14 +127,11 @@ def read_serial(ser):
             nowTime = int(time.time())
 
             # Do cool stuff            
-            if ((nowTime > timeCfC or repeatSend) and rules):
+            if (nowTime > timeCfC and rules):
 
-                f = open("/var/www/html/log.log", "a")
-                f.write("STATUS: %s (%s)\n" % (str(relesStatus), str(datetime.now())))
-                f.close()
-
-                # Update Check Time
-                timeCfC = nowTime + 30
+                f = open("/var/www/html/log.log", "a") # TMP
+                f.write("STATUS: %s (%s)\n" % (str(relesStatus), str(datetime.now()))) # TMP
+                f.close() # TMP
 
                 # Get Current Data
                 conn = MySQLdb.connect(host= "localhost", user="root", passwd="root", db="TECNOQ")
@@ -139,77 +140,95 @@ def read_serial(ser):
                 nodeUpdates = x.fetchall()
                 # print(nodeUpdates)
                 # print()
+                
+                if not updatingOtherReles:
 
-                updNODENames = []
-                if (not repeatSend):
+                    now = datetime.now()
+                    todayRel = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+
+                    # Setup Check NODE Once 
+                    updNODENames = []
+
+                    # Empty Rele-to-update list
                     mustBeUpdated = []
 
-                # Remove Old ON
-                for releN in relesStatus:
-                    for relPin in range(1, 9):
-                        pinSt = relesStatus[releN][relPin] 
-                        if pinSt[onoff] and nowTime >= pinSt[until]:
-                            pinSt[onoff] = pinSt[duration] = pinSt[until] = 0
-                            if releN not in mustBeUpdated:
-                                mustBeUpdated.append(releN)
+                    # Remove Old ON
+                    for releN in relesStatus:
+                        for relPin in range(1, 9):
+                            pinSt = relesStatus[releN][relPin] 
+                            if pinSt[onoff] and nowTime >= pinSt[until]:
+                                pinSt[onoff] = pinSt[until] = 0
+                                if releN not in mustBeUpdated:
+                                    mustBeUpdated.append(releN)
 
-                now = datetime.now()
-                todayRel = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+                    # Check Recent Tries
+                    y = conn.cursor()
+                    y.execute("SELECT * from %s where Unix > '%s' ORDER BY ID DESC" % (relestatusdb, datetime.strftime(datetime.now() - timedelta(minutes=1), "%Y-%m-%d %H:%M:%S"))) 
+                    relesRecentDB = y.fetchall()   # print(relesRecentDB)
+                    for relNam in recentTries:
+                        relTries = recentTries[relNam]
+                        if relTries > 0:
+                            isThereResponse = False
+                            for relesRecent in relesRecentDB:
+                                if relesRecent[1] == relNam:
+                                    isThereResponse = True
+                                    break
 
-                for nodeUpdate in nodeUpdates:
-                    if (nodeUpdate[1] not in updNODENames):
-                        updNODENames.append(nodeUpdate[1])
+                            if isThereResponse:
+                                recentTries[relNam] = 0
+                            else:
+                                recentTries[relNam] += 1
+                                if relTries < maxTries and relNam not in mustBeUpdated:
+                                    mustBeUpdated.append(rule.rele)
 
-                        # Analyze Data
-                        ports = {}
-                        nodeID, ports['A1'], ports['A2'], ports['A3'], ports['A4'], ports['A5'], ports['A6'], ports['B7'], ports['B8'], ports['B9'], ports['B10'], ports['B11'], ports['B12'], ports['C13'], ports['C14'], ports['C15'], ports['C16'], ports['C17'], ports['C18'], ports['C19'], ports['C20'], ports['C21'], ports['C22'], ports['D23'], ports['D24'], ports['D25'], ports['D26'], ports['D27'], ports['D28'], ports['D29'], ports['BAT30'] = nodeUpdate[1:32]
-                        for rule in rules:
-                            if nodeID == rule.node:
-                                portValue = ports[rule.port]
-                                if ((rule.compar == 'lt' and portValue < rule.value) or (rule.compar == 'gt' and portValue > rule.value)): # VALUE
-                                    if rulesTimeRange[rule.pin][startRange] <= todayRel <= rulesTimeRange[rule.pin][endRange]: # TIMERANGE
-                                        if nowTime > relesStatus[rule.rele][rule.pin][delayend]: # DELAY
-                                            
-                                            x.execute("SELECT * from %s where CID = '%s' LIMIT %s" % (nodeconvdb, rule.node, inactivityCheck))
-                                            checkNodeDatas = x.fetchall()
-                                            
-                                            # rule.port = 'B11'
-                                            activityChecked = False
-                                            portPos = 31 if rule.port == 'BAT30' else (int(rule.port[1:]) + 1)
-                                            
-                                            for checkNodeData in checkNodeDatas:
-                                                if checkNodeData[portPos] != portValue:
-                                                    activityChecked = True
-                                                    break
+                    for nodeUpdate in nodeUpdates:
+                        if (nodeUpdate[1] not in updNODENames):
+                            updNODENames.append(nodeUpdate[1])
 
-                                            if activityChecked or inactivityCheck < 3:
-                                                relesStatus[rule.rele][rule.pin][onoff] = 1
-                                                relesStatus[rule.rele][rule.pin][until] = nowTime + (rule.duration * 60)
-                                                relesStatus[rule.rele][rule.pin][duration] = max(min([rule.duration, 65535]), 1)
-                                                relesStatus[rule.rele][rule.pin][delayend] = nowTime + (rule.duration + rule.delay) * 60 
-                                                if rule.rele not in mustBeUpdated:
-                                                    mustBeUpdated.append(rule.rele)
+                            # Analyze Data
+                            ports = {}
+                            nodeID, ports['A1'], ports['A2'], ports['A3'], ports['A4'], ports['A5'], ports['A6'], ports['B7'], ports['B8'], ports['B9'], ports['B10'], ports['B11'], ports['B12'], ports['C13'], ports['C14'], ports['C15'], ports['C16'], ports['C17'], ports['C18'], ports['C19'], ports['C20'], ports['C21'], ports['C22'], ports['D23'], ports['D24'], ports['D25'], ports['D26'], ports['D27'], ports['D28'], ports['D29'], ports['BAT30'] = nodeUpdate[1:32]
+                            for rule in rules:
+                                if nodeID == rule.node:
+                                    portValue = ports[rule.port]
+                                    if ((rule.compar == 'lt' and portValue < rule.value) or (rule.compar == 'gt' and portValue > rule.value)): # VALUE
+                                        if rulesTimeRange[rule.pin][startRange] <= todayRel <= rulesTimeRange[rule.pin][endRange]: # TIMERANGE
+                                            if nowTime > relesStatus[rule.rele][rule.pin][delayend]: # DELAY
+                                                x.execute("SELECT * from %s where CID = '%s' LIMIT %s" % (nodeconvdb, rule.node, inactivityCheck))
+                                                checkNodeDatas = x.fetchall()
+                                                
+                                                # rule.port = 'B11'
+                                                activityChecked = False
+                                                portPos = 31 if rule.port == 'BAT30' else (int(rule.port[1:]) + 1)
+                                                
+                                                for checkNodeData in checkNodeDatas:
+                                                    if checkNodeData[portPos] != portValue:
+                                                        activityChecked = True
+                                                        break
 
-                # print(relesStatus)
-                # print()
-                # print(5/0)
+                                                if activityChecked or inactivityCheck < 3:
+                                                    relesStatus[rule.rele][rule.pin][onoff] = 1
+                                                    relesStatus[rule.rele][rule.pin][until] = nowTime + (rule.duration * 60)
+                                                    relesStatus[rule.rele][rule.pin][delayend] = nowTime + (rule.duration + rule.delay) * 60 
+                                                    if rule.rele not in mustBeUpdated:
+                                                        mustBeUpdated.append(rule.rele)
 
-                for releName in mustBeUpdated:
+                # print(relesStatus) # print() # print(5/0)
+
+                updatingOtherReles = False
+                if len(mustBeUpdated) > 0:
+                    releName = mustBeUpdated[0]
+                    del mustBeUpdated[0]
+
                     relStat = relesStatus[releName]
 
                     # Build Message
                     msg = ""
                     for ch in releName:
                         msg += f'{ord(ch):x}'
-                    msg += '0000'
                     
                     for pinNum, pinStat in relStat.items():
                         msg += 'ff' if pinStat[onoff] else '00' 
-                        if pinStat[duration] == 0:
-                            msg += '0000'
-                        else:
-                            msg += (f'{pinStat[duration]:x}'.zfill(4))
-                            pinStat[duration] = 0
 
                     msg += (f'{(sum(bytes.fromhex(msg))):x}'.zfill(4))
                     msg += '0ff0'    
@@ -220,12 +239,23 @@ def read_serial(ser):
                     # print(byteMsg)
                     ser.write(byteMsg)
 
-                    f = open("/var/www/html/log.log", "a")
-                    f.write("OUT: %s (%s)\n" % (msg, str(now)))
-                    f.close()
+                    recentTries[releName] += 1
+
+                    f = open("/var/www/html/log.log", "a") # TMP
+                    f.write("OUT: %s (%s)\n" % (msg, str(now))) # TMP
+                    f.close() # TMP
                 
-                repeatSend = not repeatSend
-                time.sleep(random() * 2)
+                    if len(mustBeUpdated) > 0:
+                        updatingOtherReles = True
+                
+                time.sleep(random())
+
+                # Update Check Time
+                nowTime = int(time.time())
+                if updatingOtherReles:
+                    timeCfC = nowTime + 10
+                else:
+                    timeCfC = nowTime + 30
 
         except Exception as e:
             print(e)
